bdf3f127c566ed12056a430151ed7f6e692fbb36
Kathryn Silva <kathrynsilvaconway@yahoo.com>
2022-02-15 19:25:39 -0500

    Addded two unit tests to check url and Job model. Fixed secret key leak issue.

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class URLTests(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_job(self):
+        job = Job()
+        job.job_name = "I am a test."
+        job.done = False
+        job.save()
+        record = Job.objects.get(pk=1)
+        self.assertEqual(record, job)
